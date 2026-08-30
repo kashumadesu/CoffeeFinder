b27485702ad99a08819769d2d80e80cd1f71cf32
kashumadesu <michaelapril81416@gmail.com>
2026-08-30 15:50:25 +0800

    Configure EAS environment variables and .easignore

--- babel.config.js
+++ babel.config.js
@@ -28,7 +28,7 @@ module.exports = function (api) {
           blacklist: null,
           whitelist: null,
           safe: false,
-          allowUndefined: false,
+          allowUndefined: true,
         },
       ],
     ],
